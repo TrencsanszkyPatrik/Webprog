ca6c5c53e9212711640ec17dfd516e0e9cded06f
Trencsánszky Patrik <90695000+TrencsanszkyPatrik@users.noreply.github.com>
2025-05-30 13:10:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// adatok betoltese
 async function loadData() {
     try {
         const response = await fetch('data/events.json');
@@ -8,11 +9,10 @@ async function loadData() {
         return null;
     }
 }
-
+// esemenyek szurese a kategoriák alapján
 async function filterEventsByCategory(category) {
     const data = await loadData();
     if (data) {
-        //osszes vagy kategoria szerinti gombok
         const filteredEvents = category === 'all' 
             ? data.events 
             : data.events.filter(event => event.category === category);
@@ -20,46 +20,22 @@ async function filterEventsByCategory(category) {
     }
 }
 
-//keresomezo
-function setupSearch() {
-    const searchForm = document.querySelector('form');
-    const searchInput = searchForm.querySelector('input');
-
-    searchForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const searchTerm = searchInput.value.toLowerCase(); //kisbetusit
-        const data = await loadData();
-        
-        if (data) {
-            //vizsgalja a cimet, leirast, es a tageket
-            const filteredEvents = data.events.filter(event => 
-                event.title.toLowerCase().includes(searchTerm) ||
-                event.description.toLowerCase().includes(searchTerm) ||
-                event.tags.some(tag => tag.toLowerCase().includes(searchTerm))
-            );
-            updateUI(filteredEvents, data.categories);
-        }
-    });
-}
-
 function updateUI(events, categories) {
     displayEvents(events, categories);
 }
-
-//Kategoriagombok megjelenitese
+// kategoriák megjelenitese
 function displayCategories(categories) {
     const categoriesContainer = document.getElementById('categories');
-    //eloszor az osszesgomb
     categoriesContainer.innerHTML = `
         <div class="col-6 col-sm-4 col-md-3 col-lg-1">
-            <div class="card category-card text-center p-7" data-category="all">
+            <div class="card category-card text-center p-7" onclick="filterEventsByCategory('all')">
                 <i class="bi bi-grid-3x3-gap fs-1 mb-2"></i>
                 <h5 class="card-title">Összes</h5>
             </div>
         </div>
         ${categories.map(category => `
             <div class="col-6 col-sm-4 col-md-3 col-lg-1">
-                <div class="card category-card text-center p-7" data-category="${category.name}">
+                <div class="card category-card text-center p-7" onclick="filterEventsByCategory('${category.name}')">
                     ${category.logo ? `
                         <div class="category-logo-container mb-2">
                             <img src="images/${category.logo}" alt="${category.displayName}" class="category-logo">
@@ -71,16 +47,11 @@ function displayCategories(categories) {
                 </div>
             </div>
         `).join('')}`;
-
-    document.querySelectorAll('.category-card').forEach(card => {
-        card.addEventListener('click', () => {
-            const category = card.dataset.category;
-            filterEventsByCategory(category);
-        });
-    });
 }
 
+// esemenyek megjelenitese
 function displayEvents(events, categories) {
+    console.log(events);
     const eventsContainer = document.getElementById('upcoming-events');
     eventsContainer.innerHTML = events.map(event => {
         const category = categories.find(cat => cat.name === event.category);
@@ -92,11 +63,11 @@ function displayEvents(events, categories) {
 
         const imageUrl = event.image.startsWith('http://') || event.image.startsWith('https://') ? 
                          event.image : 
-                         `images/${event.image}`;
+                         `images/${event.image}`; // vizsgaljuk hogy netrol jon az url vagy pedig mappabol
 
         return `
             <div class="col-md-6 col-lg-4">
-                <div class="card event-card" data-bs-toggle="modal" data-bs-target="#eventModal" data-event-id="${event.id}">
+                <div class="card event-card" onclick="displayEventDetails(${event.id})" data-bs-toggle="modal" data-bs-target="#eventModal" data-event-id="${event.id}">
                     <div class="position-relative">
                          <img src="${imageUrl}" class="card-img-top" alt="${event.title}">
                          ${categoryIconHtml}
@@ -117,7 +88,7 @@ function displayEvents(events, categories) {
         `;
     }).join('');
 }
-
+// hero esemeny megjelenitese
 function displayHeroEvent(event) {
     const heroSection = document.querySelector('.hero-section');
 
@@ -129,7 +100,7 @@ function displayHeroEvent(event) {
                         <h1 class="display-4">${event.title}</h1>
                         <p class="lead">${event.description}</p>
                         <p class="lead"><i class="bi bi-calendar"></i> ${event.date} <i class="bi bi-clock ms-2"></i> ${event.time}</p>
-                        <button class="btn btn-primary btn-lg mt-3" data-bs-toggle="modal" data-bs-target="#eventModal" data-event-id="${event.id}">Részletek megtekintése</button>
+                        <button class="btn btn-primary btn-lg mt-3" onclick="displayEventDetails(${event.id})" data-bs-toggle="modal" data-bs-target="#eventModal" data-event-id="${event.id}">Részletek megtekintése</button>
                     </div>
                     <div class="col-md-8">
                          <img src="${event.image.startsWith('http://') || event.image.startsWith('https://') ? 
@@ -148,38 +119,24 @@ async function initializePage() {
     const data = await loadData(); 
     
     if (data) {
-        const upcomingEvents = data.events.sort((a, b) => {
-            const dateA = new Date(`${a.date} ${a.time}`);
-            const dateB = new Date(`${b.date} ${b.time}`);
-            return dateA - dateB;
-        });
-
-        const closestEvent = upcomingEvents.length > 0 ? upcomingEvents[0] : null;
-        
-        if (closestEvent) {
-            displayHeroEvent(closestEvent);
+        if (data.events.length > 0) {
+            displayHeroEvent(data.events[0]);
         }
-
         displayCategories(data.categories);
-        displayEvents(upcomingEvents, data.categories);
-        setupSearch(); 
-        setupModal(); 
+        displayEvents(data.events, data.categories);
     }
 }
 
-function setupModal() {
-     const eventModal = document.getElementById('eventModal');
+function displayEventDetails(eventId) {
+    const eventModal = document.getElementById('eventModal');
     if (eventModal) {
-        eventModal.addEventListener('show.bs.modal', async (event) => {
-            const button = event.relatedTarget;
-            const eventId = button.getAttribute('data-event-id');
-            const data = await loadData();
-            const selectedEvent = data.events.find(e => e.id === parseInt(eventId));
+        loadData().then(data => {
+            const selectedEvent = data.events.find(e => e.id === parseInt(eventId)); // esemeny keresese az id alapjan
             const selectedOrganizer = selectedEvent.organizer;
 
             const imageUrl = selectedEvent.image.startsWith('http://') || selectedEvent.image.startsWith('https://') ? 
                          selectedEvent.image : 
-                         `images/${selectedEvent.image}`;
+                         `images/${selectedEvent.image}`; // vizsgaljuk hogy netrol jon az url vagy pedig mappabol
 
             if (selectedEvent) {
                 eventModal.querySelector('.modal-title').textContent = selectedEvent.title;
@@ -198,4 +155,23 @@ function setupModal() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', initializePage); 
+// Kereses funkcio
+function handleSearch(event) {
+    event.preventDefault();
+    const searchInput = document.querySelector('form input');
+    const searchTerm = searchInput.value.toLowerCase();
+    
+    loadData().then(data => {
+        if (data) {
+            const filteredEvents = data.events.filter(event => 
+                event.title.toLowerCase().includes(searchTerm) ||
+                event.description.toLowerCase().includes(searchTerm) ||
+                event.tags.some(tag => tag.toLowerCase().includes(searchTerm))
+            );
+            updateUI(filteredEvents, data.categories);
+        }
+    });
+}
+
+// oldal betoltese
+window.onload = initializePage; 
